6dfde2ecceac8b99b208086f7e6715962c861f01
CondorPepinillo <marttapia2023@udec.cl>
2024-06-16 16:03:20 -0400

    Se añadio un selector de calendario para escoger el dia a salir, Se hizo con la libreria jcalendar-1.4.jar

--- demo/src/main/java/com/proyecto/SelectionForm1.java
+++ demo/src/main/java/com/proyecto/SelectionForm1.java
@@ -1,17 +1,23 @@
 package com.proyecto;
 
+import com.toedter.calendar.JDateChooser;
+
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class SelectionForm1 extends JFrame {
     private JPanel panelPrincipal;
     private JComboBox origenComboBox1;
     private JComboBox destinoComboBox1;
     private JButton submit;
-    private JTextField fecha;
     private JPanel panel1;
     private JPanel labelTitulo;
     private JPanel panelConElementos;
     private JPanel panelOrigenYDestino;
+    private JPanel panelFecha;
+
+    JDateChooser dateChooser = new JDateChooser();
 
     public SelectionForm1() {
         /**
@@ -22,6 +28,9 @@ public class SelectionForm1 extends JFrame {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
+        panelFecha.add(dateChooser);
+
     }
 
     private void createUIComponents() {
--- demo/src/main/java/com/proyecto/pasajesHorariosUI.java
+++ demo/src/main/java/com/proyecto/pasajesHorariosUI.java
@@ -5,6 +5,13 @@ import java.util.Date;
 
 public class pasajesHorariosUI {
     private JPanel panel1;
+    private JLabel Caracteristicas;
+    private JButton buttonOpt1;
+    private JLabel opt1;
+    private JButton buttonOpt2;
+    private JButton buttonOpt3;
+    private JLabel opt2;
+    private JLabel opt3;
 
     private Comunas origen;
     private Comunas destino;
