edd84d7039ec4fcbd1a495cdb9fd9fbb1dd28a38
CondorPepinillo <marttapia2023@udec.cl>
2024-06-17 23:15:59 -0400

    Se creo clase bus la cual nos ayudara con la seleccion de el viaje deseado

---
+++ demo/src/main/java/com/proyecto/Bus.java
@@ -0,0 +1,9 @@
+package com.proyecto;
+
+import java.util.Date;
+
+public class Bus {
+    public Bus(Date salida, Date llegada){
+
+    }
+}
--- demo/src/main/java/com/proyecto/pasajesHorariosUI.java
+++ demo/src/main/java/com/proyecto/pasajesHorariosUI.java
@@ -1,11 +1,13 @@
 package com.proyecto;
 
 import javax.swing.*;
+import java.util.Calendar;
 import java.util.Date;
 
 public class pasajesHorariosUI {
     private JPanel panel1;
     private JLabel vainas;
+    private JPanel busesLabelsPanel;
 
     private Comunas origen;
     private Comunas destino;
@@ -22,8 +24,27 @@ public class pasajesHorariosUI {
         this.destino = destino;
         this.fecha = fecha;
 
+        Calendar salidaAux = Calendar.getInstance();
+        salidaAux.setTime(fecha);
+
+        Calendar llegadaAux = Calendar.getInstance();
+        llegadaAux.setTime(fecha);
+
+
+        for(int i = 0; i < 8; i++){
+            salidaAux.set(Calendar.HOUR_OF_DAY, 8 + 2*i);
+            salidaAux.set(Calendar.MINUTE, 0);
+            salidaAux.set(Calendar.SECOND, 0);
+            Date salida = salidaAux.getTime();
+
+            llegadaAux.set(Calendar.HOUR_OF_DAY, 10 + 2*i);
+            llegadaAux.set(Calendar.MINUTE, 0);
+            llegadaAux.set(Calendar.SECOND, 0);
+            Date llegada = llegadaAux.getTime();
+
+            System.out.println(salida);
+            System.out.println(llegada);
+            Bus bus = new Bus(salida, llegada);
+        }
     }
-
-
-
 }
