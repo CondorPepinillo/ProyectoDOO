419c5aa6f2b9237026cbc920eecbb25b809bc163
CondorPepinillo <marttapia2023@udec.cl>
2024-06-17 21:52:22 -0400

    Feat: Se añadio el que el boton buscar viaje abra otra ventana, en esta deberian apararecer todas las opciones de viaje

--- demo/src/main/java/com/proyecto/SelectionForm1.java
+++ demo/src/main/java/com/proyecto/SelectionForm1.java
@@ -5,11 +5,12 @@ import com.toedter.calendar.JDateChooser;
 import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Objects;
 
 public class SelectionForm1 extends JFrame {
     private JPanel panelPrincipal;
-    private JComboBox origenComboBox1;
-    private JComboBox destinoComboBox1;
+    private JComboBox<Comunas> origenComboBox1;
+    private JComboBox<Comunas> destinoComboBox1;
     private JButton submit;
     private JPanel panel1;
     private JPanel labelTitulo;
@@ -31,6 +32,23 @@ public class SelectionForm1 extends JFrame {
 
         panelFecha.add(dateChooser);
 
+        Comunas[] comunas = new Comunas[]{Comunas.CONCEPCION, Comunas.LOS_ANGELES, Comunas.NACIMIENTO};
+
+        for (Comunas comuna : comunas) {
+            origenComboBox1.addItem(comuna);
+            destinoComboBox1.addItem(comuna);
+        }
+
+        submit.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                if(origenComboBox1.getSelectedItem() != destinoComboBox1.getSelectedItem() && origenComboBox1 != null & destinoComboBox1 != null){
+                    pasajesHorariosUI ventanaSeleccion = new pasajesHorariosUI((Comunas) origenComboBox1.getSelectedItem(), (Comunas) destinoComboBox1.getSelectedItem(), dateChooser.getDate());
+                    frame.dispose();
+                }
+            }
+        });
     }
 
     private void createUIComponents() {
--- demo/src/main/java/com/proyecto/pasajesHorariosUI.java
+++ demo/src/main/java/com/proyecto/pasajesHorariosUI.java
@@ -5,21 +5,28 @@ import java.util.Date;
 
 public class pasajesHorariosUI {
     private JPanel panel1;
-    private JLabel Caracteristicas;
-    private JButton buttonOpt1;
-    private JLabel opt1;
-    private JButton buttonOpt2;
-    private JButton buttonOpt3;
-    private JLabel opt2;
-    private JLabel opt3;
+    private JLabel origenLabel;
+    private JLabel destinoLabel;
 
     private Comunas origen;
     private Comunas destino;
     private Date fecha;
 
     public pasajesHorariosUI(Comunas origen, Comunas destino, Date fecha){
+        JFrame frame = new JFrame("Panel Seleccion Bus");
+        frame.getContentPane().add(panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
         this.origen = origen;
         this.destino = destino;
         this.fecha = fecha;
+        System.out.println(origen.toString() + destino + fecha);
+        origenLabel.setText(origen.toString());
+        destinoLabel.setText(destino.toString());
+
     }
+
+
 }
