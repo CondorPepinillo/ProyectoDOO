3e46440729569c119e63920bb1b48386d9bcbeb4
CondorPepinillo <marttapia2023@udec.cl>
2024-06-23 13:15:53 -0400

    Se añadieron botones para seleccion de bus

--- demo/src/main/java/com/proyecto/Bus.java
+++ demo/src/main/java/com/proyecto/Bus.java
@@ -6,6 +6,26 @@ import java.util.Date;
 
 public class Bus {
     private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JButton button11;
+    private JButton button12;
+    private JButton button13;
+    private JButton button14;
+    private JButton button15;
+    private JButton button16;
+    private JButton button17;
+    private JButton button18;
+    private JButton button19;
+    private JButton button20;
 
     public Bus(){
         JFrame frame = new JFrame("Panel Principal");
