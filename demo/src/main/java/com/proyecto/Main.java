791ddfa7953a1e2c730224824526c37145179616
martinezudec <benjamartinez2023@udec.cl>
2024-07-14 19:22:41 -0400

    feat: Se comienza inicialización de asientos

--- demo/src/main/java/com/proyecto/Main.java
+++ demo/src/main/java/com/proyecto/Main.java
@@ -1,6 +1,7 @@
 package com.proyecto;
 public class  Main {
     public static void main(String[] args) {
-        SelectionForm1 ventana = new SelectionForm1();
+        BusInfo.initializeBusSeats();
+        BusInfo.startApplication();
     }
 } 
