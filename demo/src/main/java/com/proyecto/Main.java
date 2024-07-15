8b228faff3caf1d2ba4b1acd2f98eacf3e78d930
CondorPepinillo <marttapia2023@udec.cl>
2024-07-14 21:57:55 -0400

    Se elimino BusInfo puesto ya no sera nesesaria

--- demo/src/main/java/com/proyecto/Main.java
+++ demo/src/main/java/com/proyecto/Main.java
@@ -1,7 +1,6 @@
 package com.proyecto;
 public class  Main {
     public static void main(String[] args) {
-        BusInfo.initializeBusSeats();
         SelectionForm1 ventana = new SelectionForm1();
     }
 } 
